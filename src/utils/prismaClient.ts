e652c75e6b1efe9d3b6c70abe42c9d4cc93b0901
Joaquin Thiogo <joanthio09@gmail.com>
2024-06-20 17:43:11 +0700

    change prisma

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,30 +1,26 @@
 import { PrismaClient as PrismaClientDev } from "./../../prisma/generated/prisma-dev";
 import { PrismaClient as PrismaClientTest } from "./../../prisma/generated/prisma-test";
-import { log } from "./log";
 
-export const prisma = new PrismaClientDev({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-    {
-      emit: "stdout",
-      level: "error",
-    },
-    {
-      emit: "stdout",
-      level: "info",
-    },
-    {
-      emit: "stdout",
-      level: "warn",
-    },
-  ],
-});
-
-prisma.$on("query", (e: any) => {
-  log.info(e.query);
-});
-
-export const testClient = new PrismaClientTest();
+export const prisma =
+  process.env.NODE_ENV === "development"
+    ? new PrismaClientDev({
+        log: [
+          {
+            emit: "stdout",
+            level: "query",
+          },
+          {
+            emit: "stdout",
+            level: "error",
+          },
+          {
+            emit: "stdout",
+            level: "info",
+          },
+          {
+            emit: "stdout",
+            level: "warn",
+          },
+        ],
+      })
+    : new PrismaClientTest();
